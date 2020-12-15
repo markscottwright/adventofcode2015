fb8baaee5dde5bb0896fc48d1bec162843fd2cae
Mark Wright <mark@silver.(none)>
2020-12-14 21:44:12 -0500

    Day 17

---
+++ src/test/java/adventofcode2015/Day17Test.java
@@ -0,0 +1,19 @@
+package adventofcode2015;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Day17Test {
+
+    @Test
+    public void testPart1() {
+        int n = new ContainerFiller(List.of(20, 15, 10, 5, 5), 25)
+                .numValidCombinations();
+        assertThat(n).isEqualTo(4);
+    }
+
+}
