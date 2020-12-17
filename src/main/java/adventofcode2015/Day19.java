807c04ea07d9a15a6d43cca64e6cc10f3d353d59
Mark Wright <Mark.Wright@entrust.com>
2020-12-17 13:55:07 -0500

    Day 19 part 2

--- src/main/java/adventofcode2015/Day19.java
+++ src/main/java/adventofcode2015/Day19.java
@@ -1,6 +1,7 @@
 package adventofcode2015;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
@@ -14,7 +15,7 @@ public class Day19 {
         // parse input file
         boolean inRules = true;
         Map<String, TreeSet<String>> rules = new TreeMap<>();
-        String inputMolecule = "";
+        String finalMolecule = "";
         for (String l : Util.inputLinesForDay(19)) {
             if (l.isEmpty())
                 inRules = false;
@@ -25,15 +26,106 @@ public class Day19 {
                     rules.put(from, new TreeSet<>());
                 rules.get(from).add(to);
             } else
-                inputMolecule = l;
+                finalMolecule = l;
         }
 
-        ArrayList<String> moleculeParts = parseMolecule(inputMolecule);
+        // part one
+        ArrayList<String> moleculeParts = parseMolecule(finalMolecule);
         HashSet<String> allSubstitutions = findAllSingleSubstitutions(rules,
                 moleculeParts);
         System.out.println("Day 19 part 1: " + allSubstitutions.size());
-        
-        // BFS or A* for part 2?
+
+        ArrayList<String> rulesInOrder = new ArrayList<>(rules.keySet());
+        rulesInOrder
+                .sort((r1, r2) -> -Integer.compare(r1.length(), r2.length()));
+
+        // reverse rules are unique (empirically). Try to go from destination
+        // molecule back to 'e'
+        HashMap<String, String> reversedRules = new HashMap<>();
+        for (var substitutions : rules.entrySet()) {
+            for (var substitution : substitutions.getValue())
+                reversedRules.put(substitution, substitutions.getKey());
+        }
+        ArrayList<String> rulesInDescendingLengthOrder = new ArrayList<>(
+                reversedRules.keySet());
+        rulesInDescendingLengthOrder
+                .sort((r1, r2) -> -Integer.compare(r1.length(), r2.length()));
+        var transformations = findReverseRoute(rulesInDescendingLengthOrder,
+                reversedRules, finalMolecule, "e", new ArrayList<>());
+        System.out.println("Day 19 part 2: " + transformations.size());
+    }
+
+    /*
+     * this works, but I don't think it guarantees a minimum solution. I think
+     * this needs an A* solution to be actually correct. But AOC accepts it, so
+     * it meets requirements...
+     * 
+     * Go from final molecule to start ("e") by repeatedly substituting using
+     * the reverse of the rules from the input.
+     */
+    static ArrayList<String[]> findReverseRoute(
+            ArrayList<String> rulesInPreferredOrder, Map<String, String> rules,
+            String currentMolecule, String finalMolecule,
+            ArrayList<String[]> transformations) {
+        if (currentMolecule.equals(finalMolecule)) {
+            return transformations;
+        } else if (currentMolecule.length() == finalMolecule.length()) {
+            return null;
+        } else {
+            for (String rule : rulesInPreferredOrder) {
+                for (int pos = currentMolecule
+                        .indexOf(rule); pos != -1; pos = currentMolecule
+                                .indexOf(rule, pos)) {
+                    String substitution = rules.get(rule);
+                    String nextMolecule = currentMolecule.substring(0, pos)
+                            + substitution
+                            + currentMolecule.substring(pos + rule.length());
+                    transformations.add(new String[] { rule, substitution });
+                    var solution = findReverseRoute(rulesInPreferredOrder, rules,
+                            nextMolecule, finalMolecule, transformations);
+                    if (solution != null)
+                        return solution;
+                    transformations.remove(transformations.size() - 1);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Try to go forward.  This algorithm is too slow.
+     */
+    static ArrayList<String[]> findRoute1(
+            ArrayList<String> rulesInPreferredOrder,
+            Map<String, TreeSet<String>> rules, String currentMolecule,
+            String finalMolecule, ArrayList<String[]> transformations) {
+        if (currentMolecule.equals(finalMolecule)) {
+            return transformations;
+        } else if (currentMolecule.length() >= finalMolecule.length()) {
+            return null;
+        } else {
+            for (String rule : rulesInPreferredOrder) {
+                for (int pos = currentMolecule
+                        .indexOf(rule); pos != -1; pos = currentMolecule
+                                .indexOf(rule, pos)) {
+                    for (String substitution : rules.get(rule)) {
+                        String nextMolecule = currentMolecule.substring(0, pos)
+                                + substitution + currentMolecule
+                                        .substring(pos + rule.length());
+                        transformations
+                                .add(new String[] { rule, substitution });
+                        System.out.println(currentMolecule + " (" + substitution
+                                + ") " + nextMolecule);
+                        var solution = findRoute1(rulesInPreferredOrder, rules,
+                                nextMolecule, finalMolecule, transformations);
+                        if (solution != null)
+                            return solution;
+                        transformations.remove(transformations.size() - 1);
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     private static HashSet<String> findAllSingleSubstitutions(
